be61ef2d68530ccda4c94d6bf4c867b8e7527a09
MaagnyLima <you@example.com>
2022-09-28 17:33:06 -0300

    style: screen layout login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'cadastro-usuario',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,3 +1,4 @@
+import { NavController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
+  entrar(){
+    this.navCtrl.navigateRoot('cadastro-usuario');
+  }
 }
