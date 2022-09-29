31133de8c9fbb20a26eb76ebd2bb4554d47cf8a5
MaagnyLima <you@example.com>
2022-09-29 14:33:28 -0300

    feat: added list categories and functions buttons

--- src/app/registrar-lcmto/registrar-lcmto.page.ts
+++ src/app/registrar-lcmto/registrar-lcmto.page.ts
@@ -1,3 +1,4 @@
+import { NavController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar-lcmto.page.scss'],
 })
 export class RegistrarLcmtoPage implements OnInit {
+  public categoriaLan;
 
-  constructor() { }
+  constructor(public navCtrl: NavController) {
+    this.categoriaLan=[
+      {nome:'Alimentação'},
+      {nome:'Assinaturas'},
+      {nome:'Casa'},
+      {nome:'Compras'},
+      {nome:'Cuidados pessoais'},
+      {nome:'Empréstimo'},
+      {nome:'Hobbies e lazer'},
+      {nome:'Impostos e taxas'},
+      {nome:'Investimento'},
+      {nome:'Mercado'},
+      {nome:'Outros'},
+      {nome:'Salário'},
+      {nome:'Saúde'},
+      {nome:'Serviços'},
+      {nome:'Trabalho'},
+      {nome:'Transporte'},
+    ];
+  }
 
   ngOnInit() {
   }
+  registrar(){
+    this.categoriaLan('');
+  };
+  inicio(){
+    this.navCtrl.navigateRoot('inicio');
+  };
+  lancamento(){
+    this.navCtrl.navigateRoot('lancamentos');
+  };
 
 }
